afe47d36a9f3ff7d0ab110caa047a746ff488f92
Chris Matteson <chris.matteson@gmail.com>
2020-01-20 13:10:10 -0800

    add steps to install unzip

--- main.tf
+++ main.tf
@@ -16,6 +16,18 @@ resource "local_file" "userdata" {
   filename = "${path.module}/userdata.txt"
   content  = <<TEMP
 #!/bin/bash
+if ! $(command -v unzip); then
+  if $(command -v apt-get); then
+    sudo apt-get update -y
+    sudo apt-get install -y unzip
+  elif $(command -v yum); then
+    sudo yum update -y
+    sudo yum install -y unzip
+  else
+    echo "Could not find apt-get or yum"
+    exit 1
+  fi
+fi
 userdata_zip=$(cat <<EOF
 ${data.local_file.compress.content_base64}
 EOF
