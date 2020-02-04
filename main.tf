db83462d22107a5b2f02f0a02d5bc5c379a3ef34
Chris Matteson <chris.matteson@gmail.com>
2020-02-04 02:31:45 -0600

    fix issue with constant updates

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ data "archive_file" "compress" {
 
 data "local_file" "compress" {
   filename   = data.archive_file.compress.output_path
-  depends_on = [data.archive_file.compress]
 }
 
 resource "local_file" "userdata" {
